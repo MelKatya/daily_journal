f47dfc9dc234f3dd86c9e0a5351bfc071d40cf69
Katerina <mel.katya@mail.ru>
2025-07-02 15:21:03 +0300

    add create tables

--- flask-version/main.py
+++ flask-version/main.py
@@ -1,7 +1,17 @@
 from flask import Flask
+from core.models import create_tasks_table, create_users_table
 
 
 app = Flask(__name__)
+
+with app.app_context():
+    create_users_table()
+    create_tasks_table()
+
+
+@app.route("/")
+def home_page():
+    return "home"
 
 
 if __name__ == "__main__":
