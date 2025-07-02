3e8ecfd2357a69a15e6f90110343858d115a16f4
Katerina <mel.katya@mail.ru>
2025-07-02 15:20:39 +0300

    some correct

--- flask-version/core/models/task.py
+++ flask-version/core/models/task.py
@@ -1,17 +1,17 @@
-from base import db
+from .base import db
 
 
 def create_tasks_table():
     with db.connect() as cur:
         cur.execute(
             """
-            CREATE TABLE IF NOT EXIST tasks (
+            CREATE TABLE IF NOT EXISTS tasks (
                 id INT PRIMARY KEY,
-                id_users INT VARCHAR(20) references users(id),
+                id_users INT references users(id),
                 name VARCHAR(30),
                 describe TEXT,
                 completed BOOL DEFAULT false,
-                created_at TIMESTAMP DEFAULT CURRENT_TIME, 
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
                 completed_at TIMESTAMP 
             )
             """
