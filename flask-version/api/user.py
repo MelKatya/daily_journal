6b2077fc580b3de6b449013fb0acd7ba0c8a4743
Katerina <mel.katya@mail.ru>
2025-07-06 22:57:56 +0300

    add login and logout user

--- flask-version/api/user.py
+++ flask-version/api/user.py
@@ -1,7 +1,7 @@
-from flask import Blueprint, request, flash, redirect, url_for, render_template
-from core.schemas.user import RegistrationForm
+from flask import Blueprint, request, flash, redirect, url_for, render_template, session
+from core.schemas.user import RegistrationForm, LoginForm
 
-from crud.user import add_new_user
+from crud.user import add_new_user, check_user_exists
 
 app_route = Blueprint("user", __name__)
 
@@ -15,3 +15,31 @@ def registers_new_user():
         add_new_user(name, email, password)
         return f"{name}, {email}, {password}"
     return render_template("register.html", form=form)
+
+
+@app_route.route("/login", methods=["GET", "POST"])
+def login_user():
+    """Аутентифицирует пользователя"""
+    form = LoginForm(request.form)
+    if request.method == "POST" and form.validate():
+        name, password = form.name.data, form.password.data
+        if user_id := check_user_exists(name, password):
+            session['user_id'] = user_id
+            session['name'] = name
+            return f"welcome {name}"
+        else:
+            return "Wrong name or password"
+    return render_template("login.html", form=form)
+
+
+@app_route.route("/logout")
+def logout_user():
+    """Удаляет пользователя из сеанса"""
+    if "user_id" in session:
+        session.pop('user_id')
+        name = session.get('name')
+        session.pop('name')
+        return f"Bye {name}"
+    else:
+        return "Wrong name or password"
+
