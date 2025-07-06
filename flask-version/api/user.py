865394dc591da55a02ed57ffb496c533b16d72a3
Katerina <mel.katya@mail.ru>
2025-07-06 22:57:12 +0300

    rename route from user ti registration

--- flask-version/api/user.py
+++ flask-version/api/user.py
@@ -6,8 +6,9 @@ from crud.user import add_new_user
 app_route = Blueprint("user", __name__)
 
 
-@app_route.route("/users", methods=["GET", "POST"])
-def create_user():
+@app_route.route("/registration", methods=["GET", "POST"])
+def registers_new_user():
+    """Создает нового пользователя"""
     form = RegistrationForm(request.form)
     if request.method == "POST" and form.validate():
         name, email, password = form.name.data, form.email.data, form.password.data
