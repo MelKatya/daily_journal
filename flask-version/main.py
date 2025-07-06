90946593fb55f03a21caa772cc0c48bb6e1c9990
Katerina <mel.katya@mail.ru>
2025-07-06 18:03:12 +0300

    add user route

--- flask-version/main.py
+++ flask-version/main.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from core.models import create_tasks_table, create_users_table
+from api import app_user
 
 
 app = Flask(__name__)
+app.register_blueprint(app_user)
 
 with app.app_context():
     create_users_table()
