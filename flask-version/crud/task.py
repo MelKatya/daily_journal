2afc6303a5176358167081824500b210d8f1e46e
Katerina <mel.katya@mail.ru>
2025-07-09 16:22:37 +0300

    add crud get task by id

--- flask-version/crud/task.py
+++ flask-version/crud/task.py
@@ -13,7 +13,7 @@ def create_task(id_users: int, name: str, describe: str):
         )
 
 
-def get_all_tasks(user_id):
+def get_all_tasks(user_id: int):
     """Выводит информацию обо всех задачах авторизованного пользователя"""
     with db.connect_return_dict() as cur:
         cur.execute(
@@ -25,5 +25,21 @@ def get_all_tasks(user_id):
         all_tasks = cur.fetchall()
         all_tasks_dict = [dict(row) for row in all_tasks]
         return all_tasks_dict
+
+
+def get_task_by_id(user_id: int, task_id: int):
+    """Выводит информацию обо всех задачах авторизованного пользователя"""
+    with db.connect_return_dict() as cur:
+        cur.execute(
+            """
+            SELECT * FROM tasks
+            where id_users = %s and id = %s
+            """, (user_id, task_id)
+        )
+        task = cur.fetchone()
+        if not task:
+            return
+        task_dict = dict(task)
+        return task_dict
 
 
