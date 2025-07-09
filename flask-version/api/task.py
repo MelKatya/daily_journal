a23de25765f4dae3308d968bdf1dea91416701fd
Katerina <mel.katya@mail.ru>
2025-07-09 16:23:05 +0300

    add route show task by id

--- flask-version/api/task.py
+++ flask-version/api/task.py
@@ -1,9 +1,9 @@
-from flask import Blueprint, request, flash, redirect, url_for, render_template, session
+from flask import Blueprint, request, flash, redirect, url_for, render_template, session, jsonify
 
 from api.utils import check_user_login
 from core.schemas.task import CreateTaskForm
 
-from crud.task import create_task, get_all_tasks
+from crud.task import create_task, get_all_tasks, get_task_by_id
 
 app_route = Blueprint("task", __name__)
 
@@ -20,10 +20,20 @@ def create_task_route():
     return render_template("create_task.html", form=form)
 
 
-@app_route.route("/tasks", methods=["GET", "POST"])
+@app_route.route("/tasks", methods=["GET"])
 @check_user_login
 def show_all_tasks():
     """Выводит все задачи текущего пользователя"""
     tasks = get_all_tasks(user_id=session.get("user_id"))
     return tasks
+
+
+@app_route.route("/tasks/<int:task_id>", methods=["GET"])
+@check_user_login
+def show_task_by_id(task_id: int):
+    """Выводит все задачи текущего пользователя"""
+    task = get_task_by_id(user_id=session.get("user_id"), task_id=task_id)
+    if not task:
+        return jsonify(message=f"Task with id={task_id} not found"), 404
+    return task
 
