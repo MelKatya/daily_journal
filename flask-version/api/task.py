ebf271724db4cf375743ada91f49ea4add8c1d72
Katerina <mel.katya@mail.ru>
2025-07-09 16:09:21 +0300

    add route all tasks

--- flask-version/api/task.py
+++ flask-version/api/task.py
@@ -3,12 +3,12 @@ from flask import Blueprint, request, flash, redirect, url_for, render_template,
 from api.utils import check_user_login
 from core.schemas.task import CreateTaskForm
 
-from crud.task import create_task
+from crud.task import create_task, get_all_tasks
 
 app_route = Blueprint("task", __name__)
 
 
-@app_route.route("/tasks", methods=["GET", "POST"])
+@app_route.route("/tasks/create", methods=["GET", "POST"])
 @check_user_login
 def create_task_route():
     """Создает новую задачу"""
@@ -18,4 +18,12 @@ def create_task_route():
         create_task(session.get("user_id"), name, describe)
         return f"{name}, {describe}"
     return render_template("create_task.html", form=form)
+
+
+@app_route.route("/tasks", methods=["GET", "POST"])
+@check_user_login
+def show_all_tasks():
+    """Выводит все задачи текущего пользователя"""
+    tasks = get_all_tasks(user_id=session.get("user_id"))
+    return tasks
 
