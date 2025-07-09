d3dd7a44b4fa483c9612f62a14640de8d9d642d6
Katerina <mel.katya@mail.ru>
2025-07-09 18:18:42 +0300

    add route complete task

--- flask-version/api/task.py
+++ flask-version/api/task.py
@@ -3,7 +3,7 @@ from flask import Blueprint, request, flash, redirect, url_for, render_template,
 from api.utils import check_user_login
 from core.schemas.task import CreateTaskForm
 
-from crud.task import create_task, get_all_tasks, get_task_by_id
+from crud import task as tsk
 
 app_route = Blueprint("task", __name__)
 
@@ -15,7 +15,7 @@ def create_task_route():
     form = CreateTaskForm(request.form)
     if request.method == "POST" and form.validate():
         name, describe = form.name.data, form.describe.data
-        create_task(session.get("user_id"), name, describe)
+        tsk.create_task(session.get("user_id"), name, describe)
         return f"{name}, {describe}"
     return render_template("create_task.html", form=form)
 
@@ -24,16 +24,30 @@ def create_task_route():
 @check_user_login
 def show_all_tasks():
     """Выводит все задачи текущего пользователя"""
-    tasks = get_all_tasks(user_id=session.get("user_id"))
+    tasks = tsk.get_all_tasks(user_id=session.get("user_id"))
     return tasks
 
 
 @app_route.route("/tasks/<int:task_id>", methods=["GET"])
 @check_user_login
 def show_task_by_id(task_id: int):
-    """Выводит все задачи текущего пользователя"""
-    task = get_task_by_id(user_id=session.get("user_id"), task_id=task_id)
+    """Выводит информацию о задаче по id"""
+    task = tsk.get_task_by_id(user_id=session.get("user_id"), task_id=task_id)
     if not task:
         return jsonify(message=f"Task with id={task_id} not found"), 404
+    return task
+
+
+@app_route.route("/tasks/<int:task_id>/complete", methods=["GET"])
+@check_user_login
+def complete_task_by_id(task_id: int):
+    """Помечает задачу выполненной"""
+    task = tsk.get_task_by_id(user_id=session.get("user_id"), task_id=task_id)
+    if not task:
+        return jsonify(message=f"Task with id={task_id} not found"), 404
+
+    task = tsk.complete_task_by_id(user_id=session.get("user_id"), task_id=task_id)
+    if not task:
+        return jsonify(message=f"Task with id={task_id} already completed")
     return task
 
