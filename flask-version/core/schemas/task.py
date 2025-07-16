9cdfdbdeab1ed5572a7b1185b825f2c6784e8beb
Katerina <mel.katya@mail.ru>
2025-07-16 13:53:52 +0300

    change english into russian

--- flask-version/core/schemas/task.py
+++ flask-version/core/schemas/task.py
@@ -2,5 +2,5 @@ from wtforms import Form, StringField, IntegerField, validators, PasswordField
 
 
 class CreateTaskForm(Form):
-    name = StringField("Name", [validators.Length(min=4, max=25)])
-    describe = StringField("Describe", [validators.Length(min=4, max=250)])
+    name = StringField("Название", [validators.Length(min=4, max=25)])
+    describe = StringField("Описание", [validators.Length(min=4, max=250)])
