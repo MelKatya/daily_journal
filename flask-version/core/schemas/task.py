647d2fc449ac010d9c95f0a830247df3dc49b9cd
Katerina <mel.katya@mail.ru>
2025-07-30 21:22:22 +0300

    add change task form

--- flask-version/core/schemas/task.py
+++ flask-version/core/schemas/task.py
@@ -1,6 +1,10 @@
-from wtforms import Form, StringField, IntegerField, validators, PasswordField
+from wtforms import Form, StringField, IntegerField, validators, PasswordField, BooleanField
 
 
 class CreateTaskForm(Form):
     name = StringField("Название", [validators.Length(min=4, max=25)])
+    describe = StringField("Описание", [validators.Length(min=4, max=250)])
+
+
+class ChangeTaskForm(Form):
     describe = StringField("Описание", [validators.Length(min=4, max=250)])
