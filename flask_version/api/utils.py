3c67efd6d47d28591e737554f885c0283000356b
Katerina <mel.katya@mail.ru>
2025-08-04 18:54:40 +0300

    add documentation

--- flask_version/api/utils.py
+++ flask_version/api/utils.py
@@ -1,12 +1,38 @@
 from functools import wraps
+from typing import Any, Callable
 
 from core.config import settings
 from flask import redirect, session, url_for
 
 
-def check_user_login(func):
+def check_user_login(func) -> Callable:
+    """
+    Декоратор, проверяющий, авторизован ли пользователь перед
+    выполнением функции func.
+
+    Если пользователь авторизован (в сессии присутствует ключ с user_id),
+    вызывается оригинальная функция. В противном случае происходит
+    редирект на страницу логина.
+
+    Args:
+        func (Callable): функция, к которой применяется декоратор.
+
+    Returns:
+        Callable: обернутая функция, с выполненной проверкой авторизации.
+    """
+
     @wraps(func)
-    def wrapper(*args, **kwargs):
+    def wrapper(*args, **kwargs) -> Any:
+        """
+        Обертка, проверяющая наличие user_id в сессии перед вызовом функции.
+
+        Args:
+            *args: позиционные аргументы, передаваемые в исходную функцию.
+            **kwargs: именованные аргументы, передаваемые в исходную функцию.
+
+        Returns:
+            Any: результат выполнения исходной функции или редирект на страницу логина.
+        """
         if session.get(settings.users_data.user_id):
             return func(*args, **kwargs)
         else:
