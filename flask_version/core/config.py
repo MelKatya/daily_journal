aa67512b331ddeb1c4a2ae82a39785deded06be5
Katerina <mel.katya@mail.ru>
2025-08-04 20:45:55 +0300

    add documentation

--- flask_version/core/config.py
+++ flask_version/core/config.py
@@ -6,21 +6,54 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
+@dataclass
 class DatabaseConfig:
-    database = os.getenv("DATABASE")
-    user = os.getenv("USER")
-    password = os.getenv("PASSWORD")
-    host = os.getenv("HOST")
+    """
+    Конфигурация подключения к базе данных.
+
+    Attributes:
+        database (str | None): имя базы данных.
+        user (str | None): имя пользователя.
+        password (str | None): пароль пользователя.
+        host (str | None): адрес хоста базы данных.
+    """
+
+    database: str | None = os.getenv("DATABASE")
+    user: str | None = os.getenv("USER")
+    password: str | None = os.getenv("PASSWORD")
+    host: str | None = os.getenv("HOST")
 
 
 @dataclass
 class UserAuth:
+    """
+    Константы ключей для хранения данных пользователя в сессии.
+
+    Attributes:
+        user_id (str): ключ для хранения ID пользователя.
+        name (str): ключ для хранения имени пользователя.
+    """
+
     user_id: str = "user_id"
     name: str = "name"
 
 
 @dataclass
 class ParamConfig:
+    """
+    Класс-шаблон для хранения параметров сортировки/фильтрации/поиска
+
+    Attributes:
+        name (str): название параметра.
+        default_db (str | tuple): значение по умолчанию для работы с бд.
+        default_html (str | None): значение по умолчанию для отображения
+            на HTML-страница.
+        db_map (dict[str, str | tuple[str, ...]] | None):
+            сопоставление значений из HTML-страницы с выражениями для SQL.
+        html_map (dict[str, str] | None):
+            сопоставление ключей с читаемыми подписями на HTML-странице.
+    """
+
     name: str
     default_db: str | tuple
     default_html: str | None = None
@@ -30,6 +63,15 @@ class ParamConfig:
 
 @dataclass
 class AllTaskParams:
+    """
+    Хранит преднастроенные параметры для сортировки, фильтрации и поиска задач.
+
+    Attributes:
+        SORTED (ParamConfig): параметры сортировки задач.
+        FILTER (ParamConfig): параметры фильтрации задач по статусу выполнения.
+        SEARCH (ParamConfig): параметры поиска задач по имени.
+    """
+
     SORTED: ParamConfig = ParamConfig(
         name="sorted",
         default_db="created_at",
@@ -69,6 +111,16 @@ class AllTaskParams:
 
 @dataclass
 class Settings:
+    """
+    Хранит настройки для работы приложения.
+
+    Attributes:
+        db (DatabaseConfig): конфигурация бд.
+        users_data (UserAuth): ключи для хранения данных пользователя в сессии.
+        secret_key (str | None): секретный ключ для подписи сессий Flask.
+        tasks (AllTaskParams): параметры сортировки, фильтрации и поиска задач.
+    """
+
     db: DatabaseConfig = DatabaseConfig()
     users_data: UserAuth = UserAuth()
     secret_key: str | None = os.getenv("SESSION_KEY")
