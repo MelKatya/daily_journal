66065837cd85e81b815d4b34eacd8753ea14473c
Katerina <mel.katya@mail.ru>
2025-08-05 16:03:55 +0300

    add documentation

--- flask_version/core/schemas/task.py
+++ flask_version/core/schemas/task.py
@@ -2,9 +2,15 @@ from wtforms import Form, StringField, validators
 
 
 class CreateTaskForm(Form):
+    """
+    Валидирует данные формы создания задачи.
+    """
     name = StringField("Название", [validators.Length(min=4, max=25)])
     describe = StringField("Описание", [validators.Length(min=4, max=250)])
 
 
 class ChangeTaskForm(Form):
+    """
+    Валидирует данные формы изменения задачи.
+    """
     describe = StringField("Описание", [validators.Length(min=4, max=250)])
--- flask_version/core/schemas/user.py
+++ flask_version/core/schemas/user.py
@@ -2,6 +2,9 @@ from wtforms import Form, PasswordField, StringField, validators
 
 
 class RegistrationForm(Form):
+    """
+    Валидирует данные формы регистрации нового пользователя.
+    """
     name = StringField("Имя", [validators.Length(min=4, max=25)])
     email = StringField("Email", [validators.Length(min=6, max=35)])
     password = PasswordField(
@@ -15,5 +18,8 @@ class RegistrationForm(Form):
 
 
 class LoginForm(Form):
+    """
+    Валидирует данные формы авторизации пользователя.
+    """
     name = StringField("Имя", [validators.Length(min=4, max=25)])
     password = PasswordField("Пароль")
