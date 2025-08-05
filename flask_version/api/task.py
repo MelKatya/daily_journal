8dfa1e1466e1688d3859afa2d3f76e232098e658
Katerina <mel.katya@mail.ru>
2025-08-05 15:52:48 +0300

    add documentation

--- flask_version/api/task.py
+++ flask_version/api/task.py
@@ -1,7 +1,3 @@
-from api.utils import check_user_login
-from core.config import settings
-from core.schemas.task import ChangeTaskForm, CreateTaskForm
-from crud import task as tsk
 from flask import (
     Blueprint,
     redirect,
@@ -10,25 +6,45 @@ from flask import (
     session,
     url_for,
 )
+from werkzeug import Response
+
+from api.utils import check_user_login
+from core.config import settings
+from core.schemas.task import ChangeTaskForm, CreateTaskForm
+from crud import task as tsk
 
 app_route = Blueprint("task", __name__)
 
 
 @app_route.route("/tasks/create", methods=["GET", "POST"])
 @check_user_login
-def create_task_route():
+def create_task_route() -> Response | str:
     """
-    Создает новую задачу
+    Создает новую задачу.
+
+    Для GET-запроса отображает страницу с формой создания задачи.
+    Для POST-запроса обрабатывает данные формы:
+        - Проверяет валидность введенных данных.
+        - Добавляет новую задачу в бд.
+        - Перенаправляет на страницу со всем задачами пользователя.
+
+    Returns:
+        Response: редирект на страницу со всем задачами пользователя (POST).
+        str: HTML-страница с формой создания новой задачи (GET).
     """
+    user_id = session.get(settings.users_data.user_id)
+    assert isinstance(user_id, int)
+
     form = CreateTaskForm(request.form)
     if request.method == "POST" and form.validate():
-        name, describe = form.name.data, form.describe.data
+        name, describe = str(form.name.data), str(form.describe.data)
         tsk.create_task(
-            session.get(settings.users_data.user_id),
+            user_id,
             name,
             describe,
         )
         return redirect(url_for("app.task.show_all_tasks"))
+
     return render_template(
         "create_task.html",
         form=form,
@@ -37,31 +53,57 @@ def create_task_route():
 
 @app_route.route("/tasks", methods=["GET", "POST"])
 @check_user_login
-def show_all_tasks():
+def show_all_tasks() -> str:
     """
-    Выводит все задачи текущего пользователя.
-    Сортирует, фильтрует их и выполняет поиск
+    Обрабатывает запрос на просмотр задач пользователя с параметрами
+    сортировки, фильтрации и поиска.
+
+    Получает значения из URL-параметров:
+    - sorted: порядок сортировки ('up', 'down', 'name', 'completed').
+    - filter: фильтрация по статусу выполнения
+        ('all', 'completed', 'uncompleted').
+    - search: поисковый запрос по названию задачи.
+
+    В зависимости от выбранных значений подставляет соответствующие
+    SQL-параметры и возвращает HTML-страницу с отфильтрованными задачами.
+
+    Returns:
+        str: HTML-страница с задачами пользователя.
     """
+    #  Получаем параметр сортировки из URL (например, "up" или "down")
     sort_option = request.args.get(
         settings.tasks.SORTED.name, settings.tasks.SORTED.default_html
     )
+    assert isinstance(sort_option, str)
+    #  Подставляем соответствующее SQL-значение (например, "created_at DESC")
+    assert isinstance(settings.tasks.SORTED.db_map, dict)
     sorted_for_db = settings.tasks.SORTED.db_map.get(
         sort_option, settings.tasks.SORTED.default_db
     )
 
+    #  Получаем параметр фильтрации завершенности (например, "completed")
     filter_option = request.args.get(
         settings.tasks.FILTER.name, settings.tasks.FILTER.default_html
     )
+    assert isinstance(filter_option, str)
+    #  Подставляем соответствующее значение для фильтрации в БД (например, ("true",))
+    assert isinstance(settings.tasks.FILTER.db_map, dict)
     filter_for_db = settings.tasks.FILTER.db_map.get(
         filter_option, settings.tasks.FILTER.default_db
     )
+    assert isinstance(filter_for_db, tuple)
 
+    #  Получаем строку для поиска по названию задачи
     search_query = request.args.get(
         settings.tasks.SEARCH.name, settings.tasks.SEARCH.default_db
     )
+    assert isinstance(search_query, str)
 
+    user_id = session.get(settings.users_data.user_id)
+    assert isinstance(user_id, int)
+    # Получаем задачи с учетом всех параметров
     tasks = tsk.get_all_tasks(
-        user_id=session.get(settings.users_data.user_id),
+        user_id=user_id,
         sorted_for_db=sorted_for_db,
         completed=filter_for_db,
         search_query=search_query,
@@ -79,24 +121,34 @@ def show_all_tasks():
 
 @app_route.route("/tasks/<int:task_id>", methods=["GET", "POST"])
 @check_user_login
-def show_task_by_id(task_id: int):
+def show_task_by_id(task_id: int) -> str:
     """
-    Выводит информацию о задаче по id, позволяет ее изменять
+    Отображает страницу задачу по ID. Также обрабатывает редактирование задачи.
+
+    Для GET-запроса отображает страницу с формой задачи.
+    Для POST-запроса обрабатывает данные формы:
+    - Если нажата кнопка "Изменить задачу", активируется режим редактирования.
+    - Если нажата кнопка "Сохранить изменения", обновляются описание и
+        статус выполнения:
+        - describe (str): новое описание задачи (через форму).
+        - completed (str): статус выполнения ('True' или 'False').
+
+    Если задача с указанным ID не найдена, возвращает страницу ошибки.
+
+    Args:
+        task_id (int): ID задачи, которую требуется отобразить или изменить.
+
+    Returns:
+        str: HTML-страница с задачей или с сообщением об ошибке (404).
     """
     user_id = session.get(settings.users_data.user_id)
-
-    if not user_id:
-        return render_template(
-            "mistakes.html",
-            code=403,
-            message="You are not logged in",
-        )
+    assert isinstance(user_id, int)
 
     task = tsk.get_task_by_id(
         user_id=user_id,
         task_id=task_id,
     )
-    if not task:
+    if task is None:
         return render_template(
             "mistakes.html",
             code=404,
@@ -105,9 +157,11 @@ def show_task_by_id(task_id: int):
 
     form = ChangeTaskForm(request.form)
     edit_mode = False
+
     if request.method == "POST":
         if "change" in request.form:
             edit_mode = True
+
         elif "save" in request.form and form.validate():
             edit_mode = False
             describe = form.describe.data
@@ -134,12 +188,6 @@ def show_task_by_id(task_id: int):
                 user_id=user_id,
                 task_id=task_id,
             )
-            return render_template(
-                "task_id.html",
-                task=task,
-                edit_mode=edit_mode,
-                form=form,
-            )
 
     return render_template(
         "task_id.html",
@@ -149,20 +197,23 @@ def show_task_by_id(task_id: int):
     )
 
 
-@app_route.route("/tasks/<int:task_id>/before_delete", methods=["GET", "POST"])
+@app_route.route("/tasks/<int:task_id>/before_delete")
 @check_user_login
-def before_delete_task_by_id(task_id: int):
+def before_delete_task_by_id(task_id: int) -> str:
     """
-    Запрашивает подтверждение пользователя на удаление задачи
+    Отображает страницу с запросом на удаление задачи по ее ID.
+    Сохраняет флаг подтверждения удаления в сессии.
+
+    Если задача с указанным ID не найдена, возвращает страницу ошибки.
+
+    Args:
+        task_id (int): ID задачи, которую требуется удалить.
+
+    Returns:
+        str: HTML-страница с запросом или с сообщением об ошибке.
     """
     user_id = session.get(settings.users_data.user_id)
-
-    if not user_id:
-        return render_template(
-            "mistakes.html",
-            code=403,
-            message="You are not logged in",
-        )
+    assert isinstance(user_id, int)
 
     task = tsk.get_task_by_id(
         user_id=user_id,
@@ -183,11 +234,21 @@ def before_delete_task_by_id(task_id: int):
     )
 
 
-@app_route.route("/tasks/<int:task_id>/delete", methods=["GET", "POST"])
+@app_route.route("/tasks/<int:task_id>/delete")
 @check_user_login
-def delete_task_by_id(task_id: int):
+def delete_task_by_id(task_id: int) -> Response | str:
     """
-    Удаляет задачу
+    Удаляет задачу по ее ID, если предварительно было подтверждено удаление.
+
+    Проверяет наличие флага подтверждения в сессии. Если флаг не найден,
+    возвращает страницу ошибки (403). После удаления флаг удаляется из сессии.
+
+    Args:
+        task_id (int): ID задачи, которую требуется удалить.
+
+    Returns:
+        Response: редирект на страницу со всем задачами пользователя.
+        str: HTML-страница с сообщением об ошибке.
     """
     if not session.pop(f"allow_delete_{task_id}", False):
         return render_template(
@@ -197,13 +258,7 @@ def delete_task_by_id(task_id: int):
         )
 
     user_id = session.get(settings.users_data.user_id)
-
-    if not user_id:
-        return render_template(
-            "mistakes.html",
-            code=403,
-            message="You are not logged in",
-        )
+    assert isinstance(user_id, int)
 
     tsk.delete_task_by_id(
         user_id=user_id,
@@ -212,11 +267,22 @@ def delete_task_by_id(task_id: int):
     return redirect(url_for("app.task.show_all_tasks"))
 
 
-@app_route.route("/tasks/<int:task_id>/cancel_delete", methods=["GET"])
+@app_route.route("/tasks/<int:task_id>/cancel_delete")
 @check_user_login
-def cancel_delete_task_by_id(task_id: int):
+def cancel_delete_task_by_id(task_id: int) -> Response | str:
     """
-    Отменяет удаление задачи
+    Отменяет удаление задачи по ее ID.
+
+    Проверяет наличие флага подтверждения в сессии. Если флаг не найден,
+    возвращает страницу ошибки (403). При наличии флага удаляет его из сессии.
+
+    Args:
+        task_id (int): ID задачи, удаление которой нужно отменить.
+
+    Returns:
+        Response: редирект на страницу задачи, если отмена успешна.
+        str: HTML-страница с сообщением об ошибке, если подтверждение
+            отсутствует.
     """
     if not session.pop(f"allow_delete_{task_id}", False):
         return render_template(
