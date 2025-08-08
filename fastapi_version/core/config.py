d74a91694a9729c92076f6573efe6798f8ab886e
Katerina <mel.katya@mail.ru>
2025-08-08 15:59:13 +0300

    create database config

--- fastapi_version/core/config.py
+++ fastapi_version/core/config.py
@@ -12,9 +12,18 @@ class ApiPrefix(BaseModel):
     tasks: str = "/tasks"
 
 
+class DatabaseConfig(BaseSettings):
+    url: PostgresDsn
+    echo: bool = False
+    echo_pool: bool = False
+    pool_size: int = 50
+    max_overflow: int = 10
+
+
 class Settings(BaseSettings):
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
+    db: DatabaseConfig
 
 
 settings = Settings()
