60382c34a1d039e9f9882a1b8e7d8f6b1e395325
Katerina <mel.katya@mail.ru>
2025-08-08 13:53:34 +0300

    create main

---
+++ fastapi_version/main.py
@@ -0,0 +1,16 @@
+import uvicorn
+from fastapi import FastAPI
+
+from core.config import settings
+
+
+app = FastAPI()
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:main",
+        host=settings.run.host,
+        port=settings.run.port,
+        reload=True,
+    )
