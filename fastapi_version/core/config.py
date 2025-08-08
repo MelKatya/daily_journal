95aeaaa8a4f6417875488eb9877c70ff88cd6c1b
Katerina <mel.katya@mail.ru>
2025-08-08 16:49:07 +0300

    config read fro env

--- fastapi_version/core/config.py
+++ fastapi_version/core/config.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel, PostgresDsn
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class RunConfig(BaseModel):
@@ -21,6 +21,12 @@ class DatabaseConfig(BaseSettings):
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=(".env", ".env.template"),
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        env_prefix="APP_CONFIG__",
+    )
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
     db: DatabaseConfig
