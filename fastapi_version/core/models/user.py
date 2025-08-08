2fcc9187915eed271c34cefd3ca321b9ee8f96e1
Katerina <mel.katya@mail.ru>
2025-08-08 19:40:21 +0300

    create users table model

---
+++ fastapi_version/core/models/user.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+from typing import TYPE_CHECKING
+
+from sqlalchemy import func, String, VARCHAR
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from .base import Base
+
+if TYPE_CHECKING:
+    from task import Task
+
+
+class User(Base):
+
+    name: Mapped[str] = mapped_column(VARCHAR(20), nullable=False, unique=True)
+    email: Mapped[str] = mapped_column(VARCHAR(30), nullable=False)
+    hashed_password: Mapped[str] = mapped_column(VARCHAR(80), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(default=datetime.now, server_default=func.now())
+
+    # tasks: Mapped[list["Task"]] = relationship(
+    #     back_populates="users", cascade="all, delete-orphan"
+    # )
