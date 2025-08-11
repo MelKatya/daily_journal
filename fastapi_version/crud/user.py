e92c7878a699c9e2c494f42677ab65d57ada3343
Katerina <mel.katya@mail.ru>
2025-08-11 22:12:59 +0300

    add check user's name

--- fastapi_version/crud/user.py
+++ fastapi_version/crud/user.py
@@ -1,7 +1,8 @@
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.models import User
-from core.schemas.users import UserCreate
+from core.schemas.users import UserCreate, UserHashPass
 
 
 async def create_user(
@@ -15,3 +16,16 @@ async def create_user(
     session.add(user)
     await session.commit()
     return user
+
+
+async def check_name_exists(
+    session: AsyncSession,
+    username: str,
+):
+    """Проверяет существование имени пользователя"""
+    stmt = select(User).filter_by(name=username)
+    result = await session.scalars(stmt)
+    return result.one_or_none()
+
+
+
