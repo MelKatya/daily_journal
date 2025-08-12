e0a2d489e0fe443f9c0f31f7770aeb069c302022
Katerina <mel.katya@mail.ru>
2025-08-12 16:53:11 +0300

    add creation of jwt token

--- fastapi_version/security/utils.py
+++ fastapi_version/security/utils.py
@@ -1,6 +1,13 @@
+from datetime import datetime, timedelta
+
+import jwt
 from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordBearer
+
+from core.config import settings
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 
 def verify_password(plain_password, hashed_password):
@@ -9,5 +16,16 @@ def verify_password(plain_password, hashed_password):
 
 def get_password_hash(password):
     return pwd_context.hash(password)
+
+
+def create_jwt_token(user_id: int):
+    """
+    Создает jwt токен
+    """
+    expire = datetime.utcnow() + timedelta(settings.jwt.access_token_expire)
+    payload = {"user_id": user_id, "exp": expire}
+    return jwt.encode(payload, settings.jwt.secret_key, settings.jwt.algorithm)
+
+
 
 
