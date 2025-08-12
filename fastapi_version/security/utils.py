88c8ab0ec6e3531db59ca5cc6d421c3ff7e78f41
Katerina <mel.katya@mail.ru>
2025-08-12 17:14:23 +0300

    add get user_id from token

--- fastapi_version/security/utils.py
+++ fastapi_version/security/utils.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta
 
 import jwt
+from fastapi import Depends
 from passlib.context import CryptContext
 from fastapi.security import OAuth2PasswordBearer
 
@@ -27,5 +28,14 @@ def create_jwt_token(user_id: int):
     return jwt.encode(payload, settings.jwt.secret_key, settings.jwt.algorithm)
 
 
-
-
+def get_user_id_from_token(token: str = Depends(oauth2_scheme)):
+    """
+    Получает ID пользователя из токена
+    """
+    try:
+        payload = jwt.decode(token, settings.jwt.secret_key, algorithms=[settings.jwt.algorithm])
+        return payload.get("user_id")
+    except jwt.ExpiredSignatureError:
+        pass  # Обработка ошибки истечения срока действия токена
+    except jwt.InvalidTokenError:
+        pass  # Обработка ошибки недействительного токена
