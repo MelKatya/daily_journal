56a9191c89c404f38a3a43e6f99c3eee8fd31733
Katerina <mel.katya@mail.ru>
2025-08-13 23:11:01 +0300

    add get all task with sort, filter and search

--- fastapi_version/crud/task.py
+++ fastapi_version/crud/task.py
@@ -1,3 +1,4 @@
+from sqlalchemy import select, desc
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.models import Task
@@ -10,4 +11,21 @@ async def create_task(task: TaskCreate, session: AsyncSession):
     session.add(task)
     await session.commit()
     return task
+
+
+async def get_all_tasks(
+    id_users: int,
+    sorted_for_db: str,
+    completed: list[bool],
+    search_query: str,
+    session: AsyncSession
+):
+    """Возвращает список отсортированных и отфильтрованных задач.
+    Ищет задачи по названию."""
+    stmt = select(Task).\
+        where(Task.id_users == id_users, Task.completed.in_(completed), Task.name.ilike(f"%{search_query}%")).\
+        order_by(sorted_for_db)
+    result = await session.scalars(stmt)
+    return result.all()
+
 
