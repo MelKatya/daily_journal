01aa4461e4ad0bd5e8cd55ed0659703be108c5de
Katerina <mel.katya@mail.ru>
2025-08-13 23:11:53 +0300

    copy validation form from flask application

--- fastapi_version/core/schemas/users.py
+++ fastapi_version/core/schemas/users.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel, EmailStr
+from wtforms import Form, PasswordField, StringField, validators
 
 
 class UserBase(BaseModel):
@@ -21,3 +22,27 @@ class UserCreate(UserHashPass):
     email: EmailStr
 
 
+class RegistrationForm(Form):
+    """
+    Валидирует данные формы регистрации нового пользователя.
+    """
+
+    name = StringField("Имя", [validators.Length(min=4, max=25)])
+    email = StringField("Email", [validators.Length(min=6, max=35)])
+    password = PasswordField(
+        "Пароль",
+        [
+            validators.DataRequired(),
+            validators.EqualTo("confirm", message="Пароли не совпадают"),
+        ],
+    )
+    confirm = PasswordField("Повтор пароля")
+
+
+class LoginForm(Form):
+    """
+    Валидирует данные формы авторизации пользователя.
+    """
+
+    name = StringField("Имя", [validators.Length(min=4, max=25)])
+    password = PasswordField("Пароль")
