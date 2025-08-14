5ee877f0acc4a5cebbd0b2c9d21f56a3831364e5
Katerina <mel.katya@mail.ru>
2025-08-14 18:46:37 +0300

    copy validation form from flask application

--- fastapi_version/core/schemas/tasks.py
+++ fastapi_version/core/schemas/tasks.py
@@ -1,4 +1,6 @@
 from pydantic import BaseModel
+from sqlalchemy.orm import Query
+from wtforms import Form, StringField, validators
 
 
 class TaskBase(BaseModel):
@@ -13,3 +15,31 @@ class TaskGetForm(BaseModel):
 class TaskCreate(TaskBase, TaskGetForm):
     pass
 
+
+class TaskGet(TaskBase):
+    sort_option: str = "up"
+    filter_option: str = "all"
+    search_query: str = ""
+
+
+class CreateTaskForm(Form):
+    """
+    Валидирует данные формы создания задачи.
+    """
+
+    name = StringField("Название", [validators.Length(min=4, max=25)])
+    describe = StringField("Описание", [validators.Length(min=4, max=250)])
+
+
+class ChangeTaskForm(Form):
+    """
+    Валидирует данные формы изменения задачи.
+    """
+
+    describe = StringField(
+        "Описание",
+        [
+            validators.Length(min=4, max=250),
+            validators.DataRequired(),
+        ],
+    )
