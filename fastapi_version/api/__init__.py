2fdf874779e847f5642c3a795ed4124cfcbd40c0
Katerina <mel.katya@mail.ru>
2025-08-14 15:52:07 +0300

    add task router

--- fastapi_version/api/__init__.py
+++ fastapi_version/api/__init__.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
 from .users import router as users_router
+from .tasks import router as tasks_router
 
 router = APIRouter()
 router.include_router(users_router)
+router.include_router(tasks_router)
