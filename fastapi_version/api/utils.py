4af0ae7213e860406638e12e58d3b7522f3fff79
Katerina <mel.katya@mail.ru>
2025-08-14 15:50:58 +0300

    update for correct redirect

--- fastapi_version/api/utils.py
+++ fastapi_version/api/utils.py
@@ -1,4 +1,4 @@
-from fastapi import HTTPException, status, Depends, Cookie
+from fastapi import HTTPException, Depends, Cookie, Request
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.models import db_helper
@@ -7,23 +7,18 @@ from security.utils import get_user_id_from_token
 
 
 async def check_auth(
-    token=Cookie(),
+    request: Request,
+    token=Cookie(default=None),
     session: AsyncSession = Depends(db_helper.session_getter),
 ):
     """
     Проверяет, что токен есть в cookie и существование пользователя по ID.
     """
     if not token:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="invalid token",
-        )
+        raise HTTPException(status_code=303, headers={"Location": "/login"})
 
     user_id = get_user_id_from_token(token)
     if not (user := await get_user_by_id(session, user_id)):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail=f"User with id={user_id} not found",
-        )
+        raise HTTPException(status_code=303, headers={"Location": "/login"})
 
     return user
