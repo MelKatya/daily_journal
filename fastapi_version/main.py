3370a17fb10d24eb244fdba39b1b89a738ccf6f4
Katerina <mel.katya@mail.ru>
2025-08-16 23:26:20 +0300

    add exception handler for HTTPException

--- fastapi_version/main.py
+++ fastapi_version/main.py
@@ -4,6 +4,8 @@ from pathlib import Path
 import uvicorn
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import RedirectResponse, HTMLResponse
+from starlette.exceptions import HTTPException as StarletteHTTPException
 
 from core.config import settings
 from api import router as api_router
@@ -15,11 +17,37 @@ async def lifespan(app: FastAPI):
     yield
     await db_helper.dispose()
 
-base_dir = Path(__file__).resolve().parent
+
+base_dir: Path = Path(__file__).resolve().parent
+templates = settings.templates
 
 main_app = FastAPI(lifespan=lifespan)
 main_app.include_router(api_router)
-main_app.mount("/static", StaticFiles(directory=base_dir / "static"), name="static")
+main_app.mount("/static", StaticFiles(directory=base_dir / "static"), name="static",)
+
+
+@main_app.exception_handler(StarletteHTTPException)
+async def http_exception_handler(request, exc):
+    if "Location" in exc.headers:
+        return RedirectResponse(exc.headers["Location"], status_code=exc.status_code)
+
+    template_response = templates.TemplateResponse(
+        name="mistakes.html",
+        context={
+            "request": request,
+            "code": exc.status_code,
+            "message": str(exc.detail)
+        }
+    )
+    return template_response
+
+
+@main_app.get("/", response_class=HTMLResponse)
+def home_page():
+    """
+    Корневой маршрут: перенаправляет на страницу входа пользователя.
+    """
+    return RedirectResponse("/login")
 
 
 if __name__ == "__main__":
