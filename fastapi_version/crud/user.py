34fe4ff47d1c63531e55c5182e4950ac26069de9
Katerina <mel.katya@mail.ru>
2025-08-17 17:39:00 +0300

    linters worked (black, isort, flake8) + add documentation

--- fastapi_version/crud/user.py
+++ fastapi_version/crud/user.py
@@ -8,9 +8,16 @@ from core.schemas.users import UserCreate
 async def create_user(
     session: AsyncSession,
     user_create: UserCreate,
-):
+) -> User:
     """
-    Добавляет нового пользователя
+    Добавляет нового пользователя в таблицу 'users'.
+
+    Args:
+        session (AsyncSession): асинхронная сессия SQLAlchemy.
+        user_create (UserCreate): объект с данными нового пользователя.
+
+    Returns:
+        User: объект созданного пользователя с актуальными полями.
     """
     user = User(**user_create.model_dump())
     session.add(user)
@@ -21,9 +28,17 @@ async def create_user(
 async def check_name_exists(
     session: AsyncSession,
     username: str,
-):
+) -> User | None:
     """
-    Проверяет существование имени пользователя
+    Проверяет, существует ли пользователь с указанным именем.
+
+    Args:
+        session (AsyncSession): асинхронная сессия SQLAlchemy.
+        username (str): имя пользователя для проверки.
+
+    Returns:
+        User | None: объект найденного пользователя, если такой существует,
+            иначе None.
     """
     stmt = select(User).filter_by(name=username)
     result = await session.scalars(stmt)
@@ -33,8 +48,17 @@ async def check_name_exists(
 async def get_user_by_id(
     session: AsyncSession,
     user_id: int,
-):
-    """Получает пользователя по его ID."""
+) -> User | None:
+    """
+    Возвращает пользователя по указанному ID, если такой существует.
+
+    Args:
+        session (AsyncSession): асинхронная сессия SQLAlchemy.
+        user_id (int): ID пользователя для поиска.
+
+    Returns:
+        User | None: объект найденного пользователя, если найден, иначе None.
+    """
     stmt = select(User).filter_by(id=user_id)
     result = await session.scalars(stmt)
     return result.one_or_none()
